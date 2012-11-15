8cabe1a840334f807c839c0d53ce3ceec98b36e5
Lars Tobias Skjong-Børsting <larstobi@conduct.no>
2012-11-15 10:56:53 +0100

    Formatting and quote strings without variables.

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,33 +3,33 @@
 # Default parameters for the mongodb class.
 class mongodb::params{
   case $::operatingsystem {
-    centos, redhat, oel, Amazon: {
-      $baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/${::architecture}"
-      $source  = 'mongodb::sources::yum'
-      $package = 'mongodb-server'
-      $service = 'mongod'
+    'centos', 'redhat', 'oel', 'Amazon': {
+      $baseurl   = "http://downloads-distro.mongodb.org/repo/redhat/os/${::architecture}"
+      $source    = 'mongodb::sources::yum'
+      $package   = 'mongodb-server'
+      $service   = 'mongod'
       $pkg_10gen = 'mongo-10gen-server'
-      $dbpath = '/var/lib/mongodb'
-      $logpath = '/var/log/mongodb/mongodb.log'
-      $config = '/etc/mongodb.conf'
+      $dbpath    = '/var/lib/mongodb'
+      $logpath   = '/var/log/mongodb/mongodb.log'
+      $config    = '/etc/mongodb.conf'
     }
-    debian, ubuntu: {
+    'debian', 'ubuntu': {
       $locations = {
         'sysv'    => 'http://downloads-distro.mongodb.org/repo/debian-sysvinit',
         'upstart' => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart'
       }
       case $::operatingsystem {
-        debian:  { $init = 'sysv' }
-        ubuntu:  { $init = 'upstart' }
-        default: { $init = 'sysv' }
+        'debian':  { $init = 'sysv' }
+        'ubuntu':  { $init = 'upstart' }
+        default:   { $init = 'sysv' }
       }
-      $source = 'mongodb::sources::apt'
-      $package = 'mongodb'
-      $service = 'mongodb'
+      $source    = 'mongodb::sources::apt'
+      $package   = 'mongodb'
+      $service   = 'mongodb'
       $pkg_10gen = 'mongodb-10gen'
-      $dbpath = '/var/lib/mongo'
-      $logpath = '/var/log/mongo/mongod.log'
-      $config = '/etc/mongodb.conf'
+      $dbpath    = '/var/lib/mongo'
+      $logpath   = '/var/log/mongo/mongod.log'
+      $config    = '/etc/mongodb.conf'
     }
     default: {
       fail ("mongodb: ${::operatingsystem} is not supported.")
